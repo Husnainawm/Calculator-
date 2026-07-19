2a319eebe8f39cd4369224339718b73dba75e7c8
Husnain Ali <Husnainab2@gmail.com>
2026-07-19 18:55:00 +0500

    Initialize script with element selectors for calculator functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+let display = document.getElementsByClassName(".screen");
+let input = document.querySelectorAll(".number");
+let operator = document.querySelectorAll(".operator");
+let equals = document.querySelector(".equal");
+let clear = document.querySelector(".clear");
