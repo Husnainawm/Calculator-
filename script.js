1372f0695f3b287f4102d40913045823e3ee1f91
Husnain Ali <Husnainab2@gmail.com>
2026-07-19 19:03:20 +0500

    Refactor variable declarations for clarity in calculator script

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// Selecting the elements from the DOM
 let display = document.getElementsByClassName(".screen");
 let input = document.querySelectorAll(".number");
 let operator = document.querySelectorAll(".operator");
 let equals = document.querySelector(".equal");
 let clear = document.querySelector(".clear");
+
+// Initializing variables to keep track of the calculator's state
+let currentInput = '0';     
+let previousInput = '';      
+let operator = null;  
+let shouldResetScreen = false; 
